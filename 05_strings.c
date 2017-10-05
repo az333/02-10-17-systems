e9cf1d2f70d1397156dd583ad7a4364ee1c7dfb5
Asim Kapparova <akapparova@stuy.edu>
2017-10-05 11:43:47 -0400

    progress on strcat

--- 05_strings.c
+++ 05_strings.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int my_strlen( char *str);
-char * my_strcpy( char *dest, char *source );
-char * my_strncat( char *dest, char *source, int n);
-int my_strcmp( char *s1, char *s2 );
-char * my_strchr( char *s, char c );
+int my_strlen( char * str);
+
+char * my_strcpy( char * dest, char * source );
+char * my_strncat( char * dest, char * source, int n);
+
+int my_strcmp( char * s1, char * s2 );
+char * my_strchr( char * s, char c );
 
 int my_strlen (char *str) {
   int i = 0;
@@ -13,7 +15,7 @@ int my_strlen (char *str) {
   return i;
 }
 
-char * my_strcpy( char *dest, char *source ) {
+char * my_strncpy( char * dest, char * source ) {
   int i = 0;
   while (*source ++) {
     dest[i] = source[i];
@@ -21,12 +23,27 @@ char * my_strcpy( char *dest, char *source ) {
   return dest;
 }
 
+char * my_strcat( char * dest, char * source){
+  int counter1 = 0; 
+  while (dest[ counter1]){
+    counter1 ++;
+  }
+  int counter2 = 0;
+  while (source[counter2]) {
+    dest[counter1] = source[counter2];
+    counter2 ++;
+    counter1 ++;
+  }
+  return dest;
+}
+
 
 int main () {
   char *str1 = "angelicazver";
-  //printf ("%s is this length: %d\n", str1, my_strlen(str1));
+  char *cpystr1 = "angelicazver";
+  printf ("%s is this length: %d\n", str1, my_strlen(str1));
   char *str2 = "systems";
-  //my_strcpy(str1, str2);
-  //printf ("str1 is %s\n", str1);
+  my_strcat(str1, str2);
+  printf ("str1 IS %s, str2 is %s, str1 WAS %s\n", str1, str2, cpystr1);
 
 }
