cedcc08fc7ba36077a08dbd4f51e3334bcce4c42
Angelica Zverovich <azverovich@stuy.edu>
2017-10-05 15:15:24 -0400

    strcmp working

--- 05_strings.c
+++ 05_strings.c
@@ -24,15 +24,11 @@ char * my_strncpy( char * dest, char * source ) {
 }
 
 int my_strcmp(char *s1, char *s2) {
-  while (*s1++ && *s2++) {
-    printf ("s1: %c\n", *s1);
-    printf ("s2: %c\n", *s2);
-    if (*s1 != *s2) {
-      return *s1 - *s2;
-    }
-    }
-    return 0;
+  //while the characters are equal and not null, keep going
+  for (;*s1 == *s2 && *s1; s1++,s2++) {
   }
+  return *s1 - *s2;
+}
 
 char * my_strcat( char * dest, char * source){
   int counter1 = 0;
@@ -51,15 +47,15 @@ char * my_strcat( char * dest, char * source){
 int main () {
   char *str1 = "angelicazver";
   char *cpystr1 = "angelicazver";
-  printf ("%s is this length: %d\n", str1, my_strlen(str1));
+  //printf ("%s is this length: %d\n", str1, my_strlen(str1));
   char *str2 = "systems";
   char *str3 = "angelica";
-  char *str4 = "Systems";
+  char *str4 = "systemsblah";
   //my_strcpy(str1, str2);
-  printf ("%s compared to %s is %d\n", str2, str4, my_strcmp(str2, str4) );
-  printf ("%s compared to %s is %d\n", str2, str4, strcmp(str2, str4) );
+  //printf ("%s compared to %s is %d\n", str4, str2, my_strcmp(str4, str2) );
+  //printf ("%s compared to %s is %d\n", str4, str2, strcmp(str4, str2) );
   //printf ("str1 is %s\n", str1);
-  my_strcat(str1, str2);
-  printf ("str1 IS %s, str2 is %s, str1 WAS %s\n", str1, str2, cpystr1);
+  //my_strcat(str1, str2);
+  //printf ("str1 IS %s, str2 is %s, str1 WAS %s\n", str1, str2, cpystr1);
 
 }
