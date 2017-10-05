c4e30d988e5c82c767d4c2b9f423f613045d2317
Angelica Zverovich <azverovich@stuy.edu>
2017-10-05 15:29:03 -0400

    strchr works

--- 05_strings.c
+++ 05_strings.c
@@ -2,10 +2,8 @@
 #include <stdlib.h>
 
 int my_strlen( char * str);
-
-char * my_strcpy( char * dest, char * source );
-char * my_strncat( char * dest, char * source, int n);
-
+char * my_strncpy( char * dest, char * source, int n);
+char * my_strcat( char * dest, char * source);
 int my_strcmp( char * s1, char * s2 );
 char * my_strchr( char * s, char c );
 
@@ -15,7 +13,7 @@ int my_strlen (char *str) {
   return i;
 }
 
-char * my_strncpy( char * dest, char * source ) {
+char * my_strncpy( char * dest, char * source, int n) {
   int i = 0;
   while (*source ++) {
     dest[i] = source[i];
@@ -24,10 +22,20 @@ char * my_strncpy( char * dest, char * source ) {
 }
 
 int my_strcmp(char *s1, char *s2) {
-  //while the characters are equal and not null, keep going
   for (;*s1 == *s2 && *s1; s1++,s2++) {
+    //while the characters are equal and not null, keep going
   }
   return *s1 - *s2;
+}
+
+char * my_strchr( char * s, char c ) {
+  while (*s) {
+    if (*s == c) {
+      return s;
+    }
+    s ++;
+  }
+  return NULL;
 }
 
 char * my_strcat( char * dest, char * source){
@@ -47,15 +55,24 @@ char * my_strcat( char * dest, char * source){
 int main () {
   char *str1 = "angelicazver";
   char *cpystr1 = "angelicazver";
-  //printf ("%s is this length: %d\n", str1, my_strlen(str1));
   char *str2 = "systems";
   char *str3 = "angelica";
-  char *str4 = "systemsblah";
-  //my_strcpy(str1, str2);
-  //printf ("%s compared to %s is %d\n", str4, str2, my_strcmp(str4, str2) );
-  //printf ("%s compared to %s is %d\n", str4, str2, strcmp(str4, str2) );
-  //printf ("str1 is %s\n", str1);
-  //my_strcat(str1, str2);
-  //printf ("str1 IS %s, str2 is %s, str1 WAS %s\n", str1, str2, cpystr1);
+  char *str5 = "systemsblah";
+  char *str4 = "";
+
+  //strlen tests
+    //printf ("%s is this length: %d\n", str1, my_strlen(str1));
+
+  //strcmp tests
+    //printf ("%s compared to %s is %d\n", str4, str4, my_strcmp(str4, str4) );
+    //printf ("%s compared to %s is %d\n", str4, str4, strcmp(str4, str4) );
+
+
+  //strchr tests
+    printf ("My addres is:        %p\n", my_strchr(str1, 'x'));
+    printf ("The real address is: %p\n", strchr(str1, 'x'));
+  //strcat tests
+    //my_strcat(str1, str2);
+    //printf ("str1 IS %s, str2 is %s, str1 WAS %s\n", str1, str2, cpystr1);
 
 }
