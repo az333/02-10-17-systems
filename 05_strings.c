4a1f8abc76900296acfcab94fef6286ac501fcec
Angelica Zverovich <azverovich@stuy.edu>
2017-10-05 20:37:38 -0400

    working

--- 05_strings.c
+++ 05_strings.c
@@ -27,16 +27,8 @@ char * my_strncpy( char * dest, char * source, int n) {
 }
 
 char * my_strcat( char * dest, char * source){
-  int counter1 = 0;
-  while (dest[ counter1]){
-    counter1 ++;
-  }
-  int counter2 = 0;
-  while (source[counter2]) {
-    dest[counter1] = source[counter2];
-    counter2 ++;
-    counter1 ++;
-  }
+  char *lastchar = dest + my_strlen(dest);
+  my_strncpy(lastchar,source, my_strlen(source));
   return dest;
 }
 
@@ -65,10 +57,12 @@ int main () {
   char *str5 = "systemsblah";
   char *str4 = "";
 
-  char a1[] = "asimkapp     ";
-  char a2[] = "zverovich";
+  char a1[100];
+  char a2[100];
+  char a3[100];
 
-  char a1cpy[] = "asimkapp    ";
+
+  char a1cpy[] = "angelika";
   char a2cpy[] = "zverovich";
   //strlen tests
     //printf ("%s is this length: %d\n", str1, my_strlen(str1));
@@ -78,15 +72,16 @@ int main () {
     //printf ("%s compared to %s is %d\n", str4, str4, strcmp(str4, str4) );
 
   //strncpy tests
-    printf ("The original source is [%s] and now dest is [%s]\n", a2, my_strncpy(a1, a2,2));
-    printf ("The original source is [%s] and now dest is [%s]\n", a2cpy, strncpy(a1cpy, a2cpy, 2));
+    //printf ("The original source is [%s] and now dest is [%s]\n", a2, my_strncpy(a1, a2,9));
+    //printf ("The original source is [%s] and now dest is [%s]\n", a1cpy, strncpy(a1cpy, a2cpy,9));
 
   //strchr tests
     //printf ("My addres is:        %p\n", my_strchr(str1, 'x'));
     //printf ("The real address is: %p\n", strchr(str1, 'x'));
 
-  //strcat tests
-    //my_strcat(str1, str2);
-    //printf ("str1 IS %s, str2 is %s, str1 WAS %s\n", str1, str2, cpystr1);
-
+  //strcat test
+    strcpy(a1,"angelika");
+    strcpy(a2,"zverovich");
+    strcpy(a3,"asim");
+    printf ("str1 IS %s, str2 is %s, str1 WAS %s\n", my_strcat(a1, a2), a2, a1cpy);
 }
