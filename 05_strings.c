5ea9eb72276a2f9cd903ddcf0a985c65939e6208
Asim Kapparova <akapparova@stuy.edu>
2017-10-05 20:11:11 -0400

    finished strcat, almost done with strncpy

--- 05_strings.c
+++ 05_strings.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 int my_strlen( char * str);
-char * my_strncpy( char * dest, char * source, int n);
+char * my_strncpy( char * dest, const char * source, int n);
 char * my_strcat( char * dest, char * source);
 int my_strcmp( char * s1, char * s2 );
 char * my_strchr( char * s, char c );
@@ -16,8 +16,8 @@ int my_strlen (char *str) {
   }return i;
 }
 
-char * my_strncpy( char * dest, char * source, int n) {
-  char * temp = dest;
+char * my_strncpy( char * dest, const char * source, int n) {
+  /*char * temp = dest;
   for (;*source && n; temp++, source++, n--) {
     //printf ("temp: %c, source: %c\n", *temp, *source);
     *temp = *source;
@@ -25,33 +25,26 @@ char * my_strncpy( char * dest, char * source, int n) {
   while(n--) {
     *temp++ = 0;
   }
+  return dest;*/
+
+  int i = 0;
+  while (n > i){
+    dest[i]= source[i];
+    i++;
+  }
   return dest;
 }
 
 char * my_strcat( char * dest, char * source){
+
+  int len_src = my_strlen(source);
+  int len_dest = my_strlen(dest);
   int i = 0;
-  /*int counter1 = 0;
-  while (dest[ counter1]){
-    counter1 ++;
+  for (; i < len_src; i ++) {
+    dest[len_dest + i] = source[i];
   }
-  int counter2 = 0;
-  while (source[counter2]) {
-    dest[counter1] = source[counter2];
-    counter2 ++;
-    counter1 ++;
-  }
-  return dest;*/
-
-  //using lengths instead:
-  int destlen = my_strlen (dest);
-  int sourlen = my_strlen (source);
-
-  while (i < sourlen){
-    dest[destlen + i] = src[i];
-    i++;
-  } dest[destlen + sourlen] = 0; return dest;
-  
-  
+  dest[len_dest + len_src] = 0;
+  return dest;  
 }
 
 int my_strcmp(char *s1, char *s2) {
@@ -78,29 +71,30 @@ int main () {
   char *str3 = "angelica";
   char *str5 = "systemsblah";
   char *str4 = "";
-
-  char a1[] = "asimkapp     ";
+  
+  char a1[] = "asimkapp";
   char a2[] = "zverovich";
 
   char a1cpy[] = "asimkapp    ";
   char a2cpy[] = "zverovich";
+
   //strlen tests
-    //printf ("%s is this length: %d\n", str1, my_strlen(str1));
+  printf("%s is this length: %d\n", str1, my_strlen(str1));
 
   //strcmp tests
-    //printf ("%s compared to %s is %d\n", str4, str4, my_strcmp(str4, str4) );
-    //printf ("%s compared to %s is %d\n", str4, str4, strcmp(str4, str4) );
+  printf("%s compared to %s is %d\n", str4, str4, my_strcmp(str4, str4) );
+  printf("%s compared to %s is %d\n", str4, str4, strcmp(str4, str4) );
 
   //strncpy tests
-    printf ("The original source is [%s] and now dest is [%s]\n", a2, my_strncpy(a1, a2,2));
-    printf ("The original source is [%s] and now dest is [%s]\n", a2cpy, strncpy(a1cpy, a2cpy, 2));
+  printf ("The original source is [%s] and now dest is [%s]\n", a2, my_strncpy(a1, a2,2));
+  printf ("The original source is [%s] and now dest is [%s]\n", a2cpy, strncpy(a1cpy, a2cpy, 2));
 
   //strchr tests
-    //printf ("My addres is:        %p\n", my_strchr(str1, 'x'));
-    //printf ("The real address is: %p\n", strchr(str1, 'x'));
+  printf("My address is:        %p\n", my_strchr(str1, 'x'));
+  printf("The real address is: %p\n", strchr(str1, 'x'));
 
   //strcat tests
-    //my_strcat(str1, str2);
-    //printf ("str1 IS %s, str2 is %s, str1 WAS %s\n", str1, str2, cpystr1);
+  my_strcat(a1, a2);
+  printf("a1 IS %s, a2 is %s, a1 WAS %s\n", a1, a2, a1cpy);
 
 }
