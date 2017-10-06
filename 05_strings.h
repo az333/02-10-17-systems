6763339428fb4a2cec59c76aa4693a0a77afefdc
Asim Kapparova <akapparova@stuy.edu>
2017-10-06 11:09:22 -0400

    forgot to add .h file

---
+++ 05_strings.h
@@ -0,0 +1,5 @@
+int my_strlen( char * str);
+char * my_strncpy( char * dest, char * source, int n);
+char * my_strcat( char * dest, char * source);
+int my_strcmp( char * s1, char * s2 );
+char * my_strchr( char * s, char c );
